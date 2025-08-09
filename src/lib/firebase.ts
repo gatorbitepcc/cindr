177ae9e2735eb9256059792c854f0340d07a218c
Chiyan Loo <loochiyan@gmail.com>
2025-08-09 12:43:18 -0700

    pooop

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import {
 } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyChcIL19H7b20G4wf7msT-bVj3m58pTLjA",
+  authDomain: "cindr-b5c15.firebaseapp.com",
+  projectId: "cindr-b5c15",
+  storageBucket: "cindr-b5c15.firebasestorage.app",
+  messagingSenderId: "881076096652",
+  appId: "1:881076096652:web:aad43c9e8b0afa9456f5a9",
+  measurementId: "G-L0F0GM6HFR",
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
